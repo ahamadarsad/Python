3b63c03961586aad3a3213e1ba27c027f9402e57
ahamadarsad <164853046+ahamadarsad@users.noreply.github.com>
2025-03-02 11:14:08 +0530

    Update and rename ex-3[between a even and odd number].py to Ex-3[between a even and odd numbe and countr].py

--- 3--For Loop/Ex-3[between a even and odd numbe and countr].py
+++ 3--For Loop/Ex-3[between a even and odd numbe and countr].py
@@ -2,21 +2,21 @@ a=int(input("Enter a value: "))
 b=int(input("Enter b value: "))
 
 print("Even number between ",a,"to",b)
-E_count=0
+Even_count=0
 
 for i in range(a,b):
     if(i%2==0):  
         print(i)
-        E_count=E_count+1
+        Even_count=Even_count+1
         
-print("Number of Even Count =",E_count)
+print("Number of Even Count =",Even_count)
 
 print("Odd number between of ",a,"to",b)
-o_count=0
+odd_count=0
 
 for i in range(a,b):
     if(i%2!=0):  
         print(i)
-        o_count=o_count+1
+        odd_count=odd_count+1
         
-print("Number of Count Odd =",o_count)
+print("Number of Count Odd =",odd_count)
