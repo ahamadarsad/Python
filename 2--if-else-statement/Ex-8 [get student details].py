a88d637f011a67179a7da0dd9eb3b8cb8d507f1c
ahamadarsad <164853046+ahamadarsad@users.noreply.github.com>
2025-03-02 11:01:56 +0530

    Update and rename ex-8.py to Ex-8 [get student details].py

---
+++ 2--if-else-statement/Ex-8 [get student details].py
@@ -0,0 +1,11 @@
+score =int(input("Enter your score: "))
+if(score>=70):
+    print("Your are eligible")
+    name = input("Enter your Name: ")
+    rno = input("Enter your Regster number")
+    department = input("Enter your Department")
+    clgname = input("Enter your College Name: ")
+    
+else:
+    print("Your NOT eligible")
+    
