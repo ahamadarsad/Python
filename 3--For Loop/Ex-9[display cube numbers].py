65195954d36e7e4a63504cb73f823955eef5ea1a
ahamadarsad <164853046+ahamadarsad@users.noreply.github.com>
2025-03-02 11:40:00 +0530

    Update and rename ex-9[display cube numbers].py to Ex-9[display cube numbers].py

---
+++ 3--For Loop/Ex-9[display cube numbers].py
@@ -0,0 +1,4 @@
+a=int(input("Enter integer number: "))
+for i in range(1,a+1):
+    cebe=i*i*i
+    print("Number is : ",i,"and cube of the ",i,"is :",cube)
--- For Loop/ex-9[display cube numbers].py
+++
@@ -1,3 +0,0 @@
-a=int(input("Enter integer number: "))
-for i in range(1,a+1):
-    print("Number is : ",i,"and cube of the ",i,"is :",i*i*i)
